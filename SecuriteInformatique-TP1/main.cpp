149934ca78d546b666d596045fca2fa0e159a2c9
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-02-15 20:04:23 -0500

    Initial Commit

---
+++ SecuriteInformatique-TP1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SecuriteInformatique-TP1
+//
+//  Created by Clain Dimitri on 2022-02-15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
