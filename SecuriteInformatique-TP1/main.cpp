49b9b7dfa5da5b1e28b6b8c7ac9ed14b7ee1c537
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-02-16 16:00:31 -0500

    Add thread utilisation :o

--- SecuriteInformatique-TP1/main.cpp
+++ SecuriteInformatique-TP1/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <thread>
+
+void print(int n, const std::string &str)  {
+  std::string msg = std::to_string(n) + " : " + str;
+  std::cout << msg  << std::endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<std::string> s = {
+        "Educative.blog",
+        "Educative",
+        "courses",
+        "are great"
+    };
+    std::vector<std::thread> threads;
+   
+    for (int i = 0; i < s.size(); i++) {
+      threads.push_back(std::thread(print, i, s[i]));
+    }
+
+    for (auto &th : threads) {
+      th.join();
+    }
     return 0;
 }
