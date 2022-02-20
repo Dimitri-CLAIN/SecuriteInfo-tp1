21f240afa1a700b18b00a1901666a437a1ad468b
Pandatec <louis.vandenbosssche@epitech.eu>
2022-02-20 18:40:49 -0500

    [ADD] recursive generator of key

--- SecuriteInformatique-TP1/main.cpp
+++ SecuriteInformatique-TP1/main.cpp
@@ -8,85 +8,29 @@
 #include "DictionaryAttack.hpp"
 #include <iostream>
 
-//int rec(char *key, int keyN, int arrN)
-//{
-////    char arr[43] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-////        '0','1','2','3','4','5','6','7','8','9',
-////        '!','@','#','$','%','&','*'};
-//    char arr[5] {'a','b','c','d', 'e'};
-//
-//    std::cout << key << std::endl;
-//    if (std::string(key).compare("eeee") == 0)
-//        return 1;
-//    if (arrN >= 5) {
-//        return rec(key, keyN + 1, 0);
-//    }
-//    key[keyN] = arr[arrN];
-//    return rec(key, keyN, arrN + 1);
-//
-//}
-
-//void line(char *key, int i) {
-//    char arr[5] {'a','b','c','d', 'e'};
-//
-//    for (int arrI = 0; arrI != 5; arrI++) {
-//        key[i] = arr[arrI];
-//        std::cout << key << std::endl;
-//        line(key, arrI);
-//    }
-//}
-//
-//void rec(char *key)
-//{
-//
-//    std::cout << key << std::endl;
-//    for (int keyI = 0; std::string(key).compare("eeee");) {
-//        int it = 0;
-//        line(key, keyI);
-//        keyI++;
-//        it++;
-//    }
-//}
-
-int col(char *res, char *array, int i, int n) {
-    std::cout << res << std::endl;
-    if (n == 3) {
-        return 0;
+void printAllKLengthRec(char set[], std::string prefix, int n, int k)
+{
+    if (k == 0)
+    {
+        std::cout << (prefix) << std::endl;
+        return;
     }
-    res[i] = array[n];
-    return col(res, array, i, n + 1);
-}
-
-int prev(char *res, char *array) {
-    while (std::string(res).compare("aaaa") != 0) {
-        for (int m = 0; m != 4; m++) {
-            col(res, array, m, 0);
-        }
+    for (int i = 0; i < n; i++)
+    {
+        std::string newPrefix;
+        newPrefix = prefix + set[i];
+        printAllKLengthRec(set, newPrefix, n, k - 1);
     }
-    return 0;
 }
 
-int main(int argc, const char * argv[]) {
-//    DictionaryAttack dic("Hash.txt");
-//    dic.startIntoThreads("Dictionary.txt", 1);
-    char res[5] {'\0', '\0', '\0', '\0', '\0'};
-    char arr[4] {'a','b','c','\0'};
-
-    prev(res, arr);
-//    rec(res);
-    return 0;
+void printAllKLength(char set[], int k,int n)
+{
+    printAllKLengthRec(set, "", n, k);
 }
 
-// https://www.bogotobogo.com/cplusplus/C11/1_C11_creating_thread.php
-
-
-// res[8]; // mettre a null
-//
-// arr = [abcdefghijklmnopqrstuvwxyz0123456789!@#$%&*]
-// recursive
-    // if i == 8
-    //  return
-    // if n == max arr
-    //  recusive i--, 0
-    // res[i] = arr[n]
-    // recursive i, n++
+int main( int argc, char **argv) {
+    char set[44] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*'};
+    int k = int(std::strlen(set));
+    for (int i = 1; i <= 8; i++)
+        printAllKLength(set, i, k);
+}
