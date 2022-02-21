c4fdf8c17ec3dd61234ff7d9530212d2e800c20a
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-02-20 23:05:04 -0500

    Disp on find

--- SecuriteInformatique-TP1/main.cpp
+++ SecuriteInformatique-TP1/main.cpp
@@ -10,34 +10,38 @@
 #include <thread>
 
 DictionaryAttack dic("Hash.txt");
+std::vector<std::thread> threads;
 
 void printAllKLengthRec(char set[], std::string prefix, int n, int k)
 {
     if (k == 0)
     {
-//        std::cout << (prefix) << std::endl;
-        dic.isInMyHash(dic.wordToHash(prefix));
+        std::string hash = dic.wordToHash(prefix);
+        if (dic.isInMyHash(hash))
+            std::cout << prefix << " = " << hash << std::endl;
         return;
     }
     for (int i = 0; i < n; i++)
     {
         std::string newPrefix;
         newPrefix = prefix + set[i];
-        printAllKLengthRec(set, newPrefix, n, k - 1);
+        if (threads.size() <= 50)
+            threads.push_back(std::thread(&printAllKLengthRec, set, newPrefix, n, k - 1));
+        else
+            printAllKLengthRec(set, newPrefix, n, k - 1);
     }
 }
 
 void exo2() {
     char set[44] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*'};
-//    char set[5] = {'a','b','c','d', '\0'};
     int k = int(std::strlen(set));
-    std::vector<std::thread> threads;
 
     for (int i = 8; i >= 0;) {
-        if (threads.size() <= 5) {
-            threads.push_back(std::thread(&printAllKLengthRec, set, "", k, i));
+        printAllKLengthRec(set, "", k, i);
+//        if (threads.size() <= 5) {
+//            threads.push_back(std::thread(&printAllKLengthRec, set, "", k, i));
             i--;
-        }
+//        }
     }
     for (auto &th : threads) {
         if (th.joinable())
