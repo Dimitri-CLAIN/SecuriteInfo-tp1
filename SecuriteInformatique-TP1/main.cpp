45dd832153c063bf52dcc2daf2ab18335b08d8dd
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-02-21 14:13:58 -0500

    Regule Thread counter

--- SecuriteInformatique-TP1/main.cpp
+++ SecuriteInformatique-TP1/main.cpp
@@ -8,27 +8,37 @@
 #include "DictionaryAttack.hpp"
 #include <iostream>
 #include <thread>
+#include <fstream>
 
 DictionaryAttack dic("Hash.txt");
 std::vector<std::thread> threads;
+int state = 0;
 
 void printAllKLengthRec(char set[], std::string prefix, int n, int k)
 {
     if (k == 0)
     {
         std::string hash = dic.wordToHash(prefix);
-        if (dic.isInMyHash(hash))
+//        std::cout << (prefix) << std::endl;
+        if (dic.isInMyHash(hash)) {
+            std::ofstream myfile;
+            myfile.open ("result.txt");
+            myfile << prefix << " = " << hash << std::endl;
+            myfile.close();
             std::cout << prefix << " = " << hash << std::endl;
+        }
         return;
     }
     for (int i = 0; i < n; i++)
     {
         std::string newPrefix;
         newPrefix = prefix + set[i];
-        if (threads.size() <= 50)
+        if (threads.size() <= 50) {
             threads.push_back(std::thread(&printAllKLengthRec, set, newPrefix, n, k - 1));
-        else
+        } else {
             printAllKLengthRec(set, newPrefix, n, k - 1);
+            state += 1;
+        }
     }
 }
 
@@ -36,16 +46,18 @@ void exo2() {
     char set[44] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*'};
     int k = int(std::strlen(set));
 
-    for (int i = 8; i >= 0;) {
+    for (int i = 1; i <= 8; i++) {
         printAllKLengthRec(set, "", k, i);
+        for (int x = 0; x < threads.size(); x++) {
+            if (threads[x].joinable()) {
+                threads[x].join();
+                threads.erase(threads.begin() + x);
+            }
+        }
 //        if (threads.size() <= 5) {
 //            threads.push_back(std::thread(&printAllKLengthRec, set, "", k, i));
-            i--;
+//            i--;
 //        }
-    }
-    for (auto &th : threads) {
-        if (th.joinable())
-            th.join();
     }
 }
 
@@ -53,7 +65,8 @@ int main( int argc, char **argv) {
     
 
 //    dic.startIntoThreads("Dictionary.txt", 1);
-    
+
     exo2();
+//    std::cout << dic.wordToHash("aceahdhj") << std::endl;
     return 0;
 }
